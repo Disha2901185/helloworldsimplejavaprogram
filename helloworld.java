8939fa9b4c88b6774cbe7239d9adc1974a06f15e
Disha Kansal <121227195+Disha2901185@users.noreply.github.com>
2026-05-03 16:33:26 +0530

    Add devotional message to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Har Har Mahadev !!");
       System.out.println("I love u Bholenath so much and i want u will me everywhere...");
+         System.out.println("Om namoh parvati pateh har har mahadev...!!!");
     }  
 } 
