83e15d64d50a083fc3c61fbf53ed7a6e2cfe3d98
Disha Kansal <121227195+Disha2901185@users.noreply.github.com>
2026-05-03 16:28:07 +0530

    Fix formatting of output message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Har Har Mahadev !!");
-      System.out.println("I love u Bholenath so much and i want u will me everywhere ");
+      System.out.println("I love u Bholenath so much and i want u will me everywhere");
     }  
 } 
